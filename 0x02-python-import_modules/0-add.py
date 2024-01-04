c65723b83c56aca193173c6ede22939a123fb8b5
Brianoysters <brianoysters@gmail.com>
2024-01-04 08:21:38 -0800

    update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-
-c = add(a, b)
-
-print("a + b equals to," c)
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    c = add(a, b)
+    print("a + b equals to," c)
