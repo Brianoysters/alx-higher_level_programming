e81d36c17b75a0cba64ff68ba1a1e80153508d9b
Brianoysters <144411003+Brianoysters@users.noreply.github.com>
2024-01-25 01:05:42 +0300

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-
-"""Define a class Square."""
-
-
 class Square:
-    """Represent a square."""
 
     def __init__(self, size=0):
         """Initialize a new square.
@@ -19,5 +14,4 @@ class Square:
         self.__size = size
 
     def area(self):
-        """Return the current area of the square."""
         return (self.__size * self.__size)
