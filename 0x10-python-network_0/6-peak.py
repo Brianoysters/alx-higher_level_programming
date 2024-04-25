d1f71679f68317deb4ff7e4c7266c9ba2a29743a
Brianoysters <brianoysters@gmail.com>
2024-04-25 10:57:04 -0700

    find peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a peak-finding algorithm."""
+
+def find_peak(list_of_integers):
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
+        return None
+
+    min_ = len(list_of_integers)
+    if min_ == 1:
+        return list_of_integers[0]
+    elif min_ == 2:
+        return max(list_of_integers)
+
+    mid = int(min_ / 2)
+    max_ = list_of_integers[mid]
+    if max_ > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return max_
+    elif max_ < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
+
