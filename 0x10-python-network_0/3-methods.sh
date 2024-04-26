f3d611bf0ce67a1f69fd282b4066d4d333437972
Brianoysters <144411003+Brianoysters@users.noreply.github.com>
2024-04-26 22:44:48 +0300

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #this script outputs all HTTP methods the server of a specific URL will accept.
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+curl -sI "$1" | grep "Allow" | cut -f2 -d: | sed 's/^[[:space:]]//'
 
