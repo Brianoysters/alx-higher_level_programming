bfcefc3df78adae1662f8ac01b658c08b5d58cc2
Brianoysters <144411003+Brianoysters@users.noreply.github.com>
2024-04-26 22:46:26 +0300

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #Accepts URL input, sends a POST request to passed URL,and displays the response.
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
-
+curl -X POST -s -d 'email=test@gmail.com&subject=I will always be here for PLD' "$1"
