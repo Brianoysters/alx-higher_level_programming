52eb7a6b5e9b1c66a31cb36a7c3833f30e35acb4
Brianoysters <brianoysters@gmail.com>
2024-04-26 22:57:18 -0700

    edit

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Sends post request with json data passed as first argument
-curl -X POST -s -H "Content-Type: application/json" -d @"$2" "$1"
-
+curl -sH "Content-Type: application/json" -d "$(cat "$2")" "$1"
--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #Displays the server response message
-curl -X PUT -sL -d 'You got me!' 0.0.0.0:5000/catch_me | grep "Message" | cut -f2 -d\' | sed 's/PUT.*//'
+curl -X PUT -sL -d 'You got me!' 0.0.0.0:5000/catch_me
+| grep "Message" | cut -f2 -d\' | sed 's/PUT.*//'
